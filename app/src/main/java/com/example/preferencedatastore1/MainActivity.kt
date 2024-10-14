c86d270102639d3d829710a4414a29b474fe06f6
Allan Mathenge <allanmathenge67@gmail.com>
2024-10-14 14:49:19 +0300

    set up preference data store to read and write values

--- app/src/main/java/com/example/preferencedatastore1/MainActivity.kt
+++ app/src/main/java/com/example/preferencedatastore1/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.preferencedatastore1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.preferencedatastore1.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.first
 
+// At the top level of your kotlin file:
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +32,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private suspend fun save(key: String, value: String){
+
+        val dataStoreKey = stringPreferencesKey(key)
+        dataStore.edit {settings->
+            settings[dataStoreKey]=value
+        }
+
+    }
+
+    private suspend fun read(key: String): String?{
+
+        val dataStoreKey = stringPreferencesKey(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+
     }
 
     override fun onDestroy() {
